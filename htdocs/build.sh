e0eadfde6f827bef4b0dec7bb80c615598e802d9
mvirkkil <mvirkkil@3b9c47fb-4cef-0310-ab03-83bda6f752f3>
2005-02-06 22:52:56 +0000

    Added some checking and information

--- htdocs/build.sh
+++ htdocs/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ `pwd|sed 's/.*\///'` = 'htdocs' ] 
+then 
+	echo "Create a symlink in the parent directory and rund from there instead"
+	exit
+fi
+
 if [ -d "htdocs" ]
 then
 	svn update htdocs
@@ -12,15 +18,19 @@ xsltproc --xinclude htdocs/xslt/builder.xsl htdocs/xml/site.xml
 
 if [ ! -L "style" ] 
 then
+	echo "Creating symlink for style"
 	ln -s htdocs/style `pwd`/style
 fi
 if [ ! -L "images" ] 
 then
+	echo "Creating symlink for style"
 	ln -s htdocs/images `pwd`/images
 fi
 if [ ! -L "build.sh" ]
 then
+	echo "Removing old build.sh"
 	rm build.sh
+	echo "Symlinkin to new build.sh"
 	ln -s htdocs/build.sh ./build.sh
 fi
 
