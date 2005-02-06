88095213b32439919c01e14ffcfc6acecad3b81d
mvirkkil <mvirkkil@3b9c47fb-4cef-0310-ab03-83bda6f752f3>
2005-02-06 23:09:59 +0000

    Rewrite htdocs as website

--- website/build.sh
+++ website/build.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-if [ `pwd|sed 's/.*\///'` = 'htdocs' ] 
+if [ `pwd|sed 's/.*\///'` = 'website' ] 
 then 
 	echo "Create a symlink in the parent directory and rund from there instead"
 	exit
 fi
 
-if [ -d "htdocs" ]
+if [ -d "website" ]
 then
-	svn update htdocs
+	svn update website
 else 
-	svn checkout svn://svn.berlios.de/websane/htdocs
-	chmod a+x htdocs/build.sh
+	svn checkout svn://svn.berlios.de/websane/website
+	chmod a+x website/build.sh
 fi
 
-xsltproc --xinclude htdocs/xslt/builder.xsl htdocs/xml/site.xml
+xsltproc --xinclude website/xslt/builder.xsl website/xml/site.xml
 
 if [ ! -L "style" ] 
 then
 	echo "Creating symlink for style"
-	ln -s htdocs/style `pwd`/style
+	ln -s website/style `pwd`/style
 fi
 if [ ! -L "images" ] 
 then
 	echo "Creating symlink for style"
-	ln -s htdocs/images `pwd`/images
+	ln -s website/images `pwd`/images
 fi
 if [ ! -L "build.sh" ]
 then
 	echo "Removing old build.sh"
 	rm build.sh
 	echo "Symlinkin to new build.sh"
-	ln -s htdocs/build.sh ./build.sh
+	ln -s website/build.sh ./build.sh
 fi
 
