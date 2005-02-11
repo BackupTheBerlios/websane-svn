256167ccfd00823bf1c0f857b47b2225b7160b51
mvirkkil <mvirkkil@3b9c47fb-4cef-0310-ab03-83bda6f752f3>
2005-02-11 11:47:15 +0000

    Minor changes

--- src/websane.py
+++ src/websane.py
@@ -5,12 +5,15 @@ from xml.dom import implementation
 from xml.dom.ext.reader import Sax2
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
+basepath='/home/mikko/websane_temp/websane/demo'
+
+
+
 class ReqHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		try:
 			
 			if self.path.endswith('.xhtml'):
-				basepath='/home/mikko/websane_temp/websane/demo'
 				f=open(basepath+self.path)
 				self.send_response(200)
 				self.send_header('Content-type','text/html')
@@ -24,19 +27,19 @@ class ReqHandler(BaseHTTPRequestHandler):
 			elif self.path=='/favicon.ico':
 				self.send_error(404, 'No favicon')
 			else:
-				basepath='/home/mikko/websane_temp/websane/demo'
+				
 				print basepath+self.path
 				f=open(basepath+self.path)
 				self.send_response(200)
 				
-				if getContentType( self.path ) == None:
+				if self.getContentType() == None:
 					#Let's not serve unknown stuff
 					self.send_header('Content-type','text/plain')
 					self.end_headers()
 					f.close()
 					return
 				else:
-					self.send_header('Content-type',getContentType( self.path ))
+					self.send_header('Content-type',self.getContentType())
 					
 				self.end_headers()
 				self.wfile.write(f.read())
@@ -45,24 +48,25 @@ class ReqHandler(BaseHTTPRequestHandler):
 			self.send_error(404, 'IOError')
 			
 	def do_POST(self):
+		print "sinulle on postia!"
 		return
 
+		
+	def getContentType(self):
+		if self.path.endswith('.png'):
+			return 'image/png'
+		elif self.path.endswith('.gif'):
+			return 'image/gif'
+		elif self.path.endswith('.jpg') | self.path.endswith('.jpeg'):
+			return 'image/jpeg'
+		elif self.path.endswith('.css'):
+			return 'text/css'
+		elif self.path.endswith('.js'):
+			return 'text/plain'
+		elif self.path.endswith('.html'):
+			return 'text/html'
 
 
-
-def getContentType(urlpath):
-	if urlpath.endswith('.png'):
-		return 'image/png'
-	elif urlpath.endswith('.gif'):
-		return 'image/gif'
-	elif urlpath.endswith('.jpg') | urlpath.endswith('.jpeg'):
-		return 'image/jpeg'
-	elif urlpath.endswith('.css'):
-		return 'text/css'
-	elif urlpath.endswith('.js'):
-		return 'text/plain'
-	elif urlpath.endswith('.html'):
-		return 'text/html'
 
 
 
