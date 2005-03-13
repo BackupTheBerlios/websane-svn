db3bf44a461af806245840a6c72f3a8c348b5f16
mvirkkil <mvirkkil@3b9c47fb-4cef-0310-ab03-83bda6f752f3>
2005-03-13 18:46:23 +0000

    First try to get some filehandling

---
+++ trunk/src/filehandler.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: UTF8 -*-
+# Copyright (C) 2005: Mikko Virkkilä
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+import ConfigParser
+from time import time
+import tempfile
+
+class FileHandler:
+	openfiles={}
+	
+	def exists(self, filename):
+		if self.openfiles == None:
+			return False
+		try:
+			self.openfiles[filename]
+			return True
+		except KeyError:
+			return False
+		
+		
+	def createFile(self,filename,contentType):
+		file=TemporaryFile()
+		self.openfiles[filename]=file
+		return file
+		
+	def getContentType(self,filename):
+		return self.openfiles[filename]
+		
+	def loadFile(self,filename):
+		return self.openfiles[filename]
+		
+	def deleteFile(self, filename):
+		self.openfiles[filename].close()
+		del self.openfiles[filename]
+	
+def main():
+	print "No tests"
+	
+	
+	
+if __name__ == '__main__':
+	main()
+
